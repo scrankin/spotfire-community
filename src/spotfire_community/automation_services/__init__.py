82d08f58455223381b79b5d4694d1ed676bc848c
Clay Rankin <clay@wiserocksoftware.com>
2025-08-30 18:40:43 +0000

    added missing imports

--- src/spotfire_community/automation_services/__init__.py
+++ src/spotfire_community/automation_services/__init__.py
@@ -1,9 +1,12 @@
 from .client import AutomationServicesClient
+from .models import ExecutionStatus, ExecutionStatusResponse
 from ._xml import JobDefinition, Task, OpenAnalysisTask, ApplyBookmarkTask
 
 
 __all__ = [
     "AutomationServicesClient",
+    "ExecutionStatus",
+    "ExecutionStatusResponse",
     "JobDefinition",
     "Task",
     "OpenAnalysisTask",
