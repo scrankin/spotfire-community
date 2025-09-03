f37ec1b1d484a96e41c6edaa5145b8eb08989c2a
Clay Rankin <47068439+scrankin@users.noreply.github.com>
2025-09-03 13:28:21 -0500

    added finding dashboards in a folder (#13)

--- src/spotfire_community/_core/rest/models.py
+++ src/spotfire_community/_core/rest/models.py
@@ -1,6 +1,7 @@
 """Shared REST models used by Spotfire client code."""
 
 from enum import StrEnum
+from pydantic import BaseModel, ConfigDict
 
 
 class Scope(StrEnum):
@@ -15,3 +16,26 @@ class Scope(StrEnum):
     LIBRARY_READ = "api.library.read"
     LIBRARY_WRITE = "api.library.write"
     AUTOMATION_SERVICES_EXECUTE = "api.rest.automation-services-job.execute"
+
+
+class User(BaseModel):
+    """
+    Model representing a Spotfire user.
+
+    Attributes:
+        id (str): The unique identifier of the user.
+        name (str): The name of the user.
+        domain_name (str): The domain name of the user.
+        display_name (str): The display name of the user.
+    """
+
+    model_config = ConfigDict(
+        alias_generator=lambda s: s.split("_")[0]
+        + "".join(part.title() for part in s.split("_")[1:]),
+        populate_by_name=True,
+    )
+
+    id: str
+    name: str
+    domain_name: str
+    display_name: str
